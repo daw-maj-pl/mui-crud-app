0d9a477cd2af897d46ae7dd1b1455e66501c79a6
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-20 18:29:22 +0200

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import {
+  AppBar,
+  Badge,
+  Grid,
+  IconButton,
+  InputBase,
+  Toolbar
+} from '@material-ui/core';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid container>
+          <Grid item sm={6}>
+            <InputBase />
+          </Grid>
+          <Grid item sm></Grid>
+          <Grid item sm={6}>
+            <IconButton>
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsNoneIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={4} color="primary">
+                <ChatBubbleOutlineIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <PowerSettingsNewIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
