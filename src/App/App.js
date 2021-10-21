383a583e79e174d2e824a75433f9dd84308a9adf
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-21 14:13:18 +0200

    Customize default theme

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,28 @@
 import { CssBaseline } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  makeStyles,
+  createTheme,
+  ThemeProvider
+} from '@material-ui/core/styles';
 import Header from '../components/Header';
 import SideMenu from '../components/SideMenu';
 import './App.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#333996',
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: '#f83245',
+      light: '#f8324526'
+    },
+    background: {
+      default: '#f4f5fd'
+    }
+  }
+});
 
 const useStyles = makeStyles({
   appMain: {
@@ -14,13 +34,13 @@ const useStyles = makeStyles({
 const App = () => {
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 };
 
