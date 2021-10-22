3673a02038fa764b376e14f8f996e5b64273ce2f
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-22 13:44:35 +0200

    Add group of Radio buttons for gender input

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react';
-import { TextField, Grid } from '@material-ui/core';
+import {
+  TextField,
+  Grid,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useForm, Form } from '../../components/useForm';
 
@@ -36,7 +44,29 @@ const EmployeeForm = () => {
             value={values.email}
           />
         </Grid>
-        <Grid item xs={6}></Grid>
+        <Grid item xs={6}>
+          <FormControl>
+            <FormLabel>Gender</FormLabel>
+            <RadioGroup
+              row
+              name="gender"
+              value={values.gender}
+              onChange={handleInputChange}
+            >
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
+              <FormControlLabel
+                value="other"
+                control={<Radio />}
+                label="Other"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
       </Grid>
     </Form>
   );
