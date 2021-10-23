c451d868dc8504337bf65e01abb64c97a6ccb086
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-23 11:52:26 +0200

    Create reusable Checkbox component

---
+++ src/components/controls/Checkbox.js
@@ -0,0 +1,36 @@
+import {
+  FormControl,
+  FormControlLabel,
+  Checkbox as MuiCheckbox
+} from '@material-ui/core';
+
+const Checkbox = props => {
+  const { name, label, value, onChange } = props;
+
+  const convertToDefaultEventParam = (name, value) => ({
+    target: {
+      name,
+      value
+    }
+  });
+
+  return (
+    <FormControl>
+      <FormControlLabel
+        control={
+          <MuiCheckbox
+            name={name}
+            color="primary"
+            checked={value}
+            onChange={e =>
+              onChange(convertToDefaultEventParam(name, e.target.checked))
+            }
+          />
+        }
+        label={label}
+      />
+    </FormControl>
+  );
+};
+
+export default Checkbox;
