8312157ffc52410c1aef3760e5f39132b8a56d4b
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-24 17:29:07 +0200

    Create useTable

---
+++ src/components/useTable.js
@@ -0,0 +1,9 @@
+import { Table } from '@material-ui/core';
+
+const useTable = (records, headCells) => {
+  const TblContainer = props => <Table>{props.children}</Table>;
+
+  return { TblContainer };
+};
+
+export default useTable;
