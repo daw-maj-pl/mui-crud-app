8b72c2823e0ae1ead06838f253581070c404341d
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-25 15:53:35 +0200

    Add table sorting

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -5,7 +5,8 @@ import {
   TableHead,
   TableRow,
   makeStyles,
-  TablePagination
+  TablePagination,
+  TableSortLabel
 } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
@@ -31,20 +32,45 @@ const useTable = (records, headCells) => {
   const pages = [5, 10, 25];
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(pages[page]);
+  const [order, setOrder] = useState();
+  const [orderBy, setOrderBy] = useState();
 
   const TblContainer = props => (
     <Table className={classes.table}>{props.children}</Table>
   );
 
-  const TblHead = props => (
-    <TableHead>
-      <TableRow>
-        {headCells.map(headCell => (
-          <TableCell key={headCell.id}>{headCell.label}</TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
+  const TblHead = props => {
+    const handleSortRequest = cellId => {
+      const isAsc = orderBy === cellId && order === 'asc';
+      setOrder(isAsc ? 'desc' : 'asc');
+      setOrderBy(cellId);
+    };
+
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map(headCell => (
+            <TableCell
+              key={headCell.id}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              {headCell.disableSorting ? (
+                headCell.label
+              ) : (
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : 'asc'}
+                  onClick={() => handleSortRequest(headCell.id)}
+                >
+                  {headCell.label}
+                </TableSortLabel>
+              )}
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  };
 
   const handlePageChange = (event, newPage) => {
     setPage(newPage);
@@ -67,8 +93,37 @@ const useTable = (records, headCells) => {
     />
   );
 
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map(el => el[0]);
+  };
+
+  const getComparator = (order, orderBy) => {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  };
+
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+
   const recordsAfterPagingAndSorting = () => {
-    return records.slice(page * rowsPerPage, (page + 1) * rowsPerPage);
+    return stableSort(records, getComparator(order, orderBy)).slice(
+      page * rowsPerPage,
+      (page + 1) * rowsPerPage
+    );
   };
 
   return { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting };
