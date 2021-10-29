5bb888151ab903c3e84ead75e06008f7861a43ee
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-10-29 16:49:29 +0200

    Add onClick handlers, DialogTitle and some styling

--- src/components/ConfirmDialog.js
+++ src/components/ConfirmDialog.js
@@ -3,21 +3,66 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  IconButton,
+  makeStyles,
   Typography
 } from '@material-ui/core';
 import Controls from './controls/Controls';
+import NotListedLocationIcon from '@material-ui/icons/NotListedLocation';
+
+const useStyles = makeStyles(theme => ({
+  dialog: {
+    padding: theme.spacing(2),
+    position: 'absolute',
+    top: theme.spacing(5)
+  },
+  dialogTitle: {
+    textAlign: 'center'
+  },
+  dialogContent: {
+    textAlign: 'center'
+  },
+  dialogAction: {
+    justifyContent: 'center'
+  },
+  titleIcon: {
+    backgroundColor: theme.palette.secondary.light,
+    color: theme.palette.secondary.main,
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light,
+      cursor: 'default'
+    },
+    '& .MuiSvgIcon-root': {
+      fontSize: '8rem'
+    }
+  }
+}));
 
 const ConfirmDialog = ({ confirmDialog, setConfirmDialog }) => {
+  const classes = useStyles();
+
   return (
-    <Dialog open={confirmDialog.isOpen}>
-      <DialogTitle></DialogTitle>
-      <DialogContent>
+    <Dialog open={confirmDialog.isOpen} classes={{ paper: classes.dialog }}>
+      <DialogTitle className={classes.dialogTitle}>
+        <IconButton disableRipple className={classes.titleIcon}>
+          <NotListedLocationIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent className={classes.dialogContent}>
         <Typography variant="h6">{confirmDialog.title}</Typography>
         <Typography variant="subtitle2">{confirmDialog.subTitle}</Typography>
       </DialogContent>
-      <DialogActions>
-        <Controls.Button text="No" color="default" />
-        <Controls.Button text="Yes" color="secondary" />
+      <DialogActions className={classes.dialogAction}>
+        <Controls.Button
+          text="No"
+          color="default"
+          onClick={() => setConfirmDialog({ ...confirmDialog, isOpen: false })}
+        />
+        <Controls.Button
+          text="Yes"
+          color="secondary"
+          onClick={confirmDialog.onConfirm}
+        />
       </DialogActions>
     </Dialog>
   );
